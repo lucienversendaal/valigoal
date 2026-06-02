fc94fa46aaf06526031da5535d67777160bd1bd2
lucienversendaal <lucienversendaal07@gmail.com>
2026-06-02 13:56:42 +0200

    add HorizonServiceProvider.php

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -29,7 +29,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     {
         Gate::define('viewHorizon', function ($user = null) {
             return in_array(optional($user)->email, [
-                'lucien@valicare.nl'
+                'lucien@valicare.nl',
             ]);
         });
     }
